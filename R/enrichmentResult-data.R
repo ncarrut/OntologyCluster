6860ebaf90934504d409033d8db90ca606be7281
ncarrut <carruthers.nicholas@gmail.com>
2020-11-18 21:38:41 -0500

    included real data

---
+++ R/enrichmentResult-data.R
@@ -0,0 +1,21 @@
+#' Proteomic Functional Enrichment Result
+#'
+#' Bacterial proteomic data in response to an antibiotic were submitted to the
+#' \href{https://string-db.org/}{String Database} functional enrichment tool.
+#' This file contains the network neighborhood enrichment data.
+#'
+#' @docType data
+#'
+#' @usage data(enrichmentResult)
+#'
+#' @format data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Carruthers et al. (2020) Pathogens 18;9(9):766
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/32962036/}{PubMed})
+#'
+#' @examples
+#' data(enrichmentResult)
+#'
+"enrichmentResult"
---
+++ R/membershipList-data.R
@@ -0,0 +1,22 @@
+#' Enriched Category Membership
+#'
+#' Bacterial proteomic data in response to an antibiotic were submitted to the
+#' \href{https://string-db.org/}{String Database} functional enrichment tool.
+#' Enriched network neighborhood output were converted to a list:  each element
+#' contains a vector of gene names found in that category.
+#'
+#' @docType data
+#'
+#' @usage data(membershipList)
+#'
+#' @format List
+#'
+#' @keywords datasets
+#'
+#' @references Carruthers et al. (2020) Pathogens 18;9(9):766
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/32962036/}{PubMed})
+#'
+#' @examples
+#' data(membershipList)
+#'
+"membershipList"
